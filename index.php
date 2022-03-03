f7b15e2007d64a6591f359cf98b971fdf535e38a
Adner16 <giovanni.renda16@gmail.com>
2022-03-03 16:59:05 +0100

    added script

--- index.php
+++ index.php
@@ -1,30 +1,74 @@
 <?php
 
-$subscription = false;
-
 class Products {
     public $name;
     public $barCode;
     public $price;
     public $discount;
     public $finalPice;
-    public function setSconto($subscription){
-        if($this->subcription == true){
-            $this->discount = 20;
-        }
-    }
-    public function getPrice($discount,$price){
-        return $this->finalPrice = $this->price *($discount/100);
-    }
-    function __construct($name, $barcode, $price, $discount){
+
+    function __construct($name, $barcode, $price){
         $this->name = $name;
         $this->barcode = $barCode;
         $this->price = $price;
-        $this->discount = $discount;
+        $this->discount = 0;
+    }
+    public function setSconto($subscription){
+        if($subcription == true){
+            $this->discount = 20;
+        }
+    }
+    public function getPrice(){
+         $this->finalPrice = $this->price *(1 - ($this->discount/100));
+         return $this->finalPice;
+    }
+    
+}
+
+class Food extends Products {
+    public $deadline;
+    public $species;
+
+    function __construct($name, $barcode, $price, $deadline, $species){
+        parent::__construct($name, $barcode, $price);
+        $this->deadline = $deadline;
+        $this->species = $species;
     }
 }
 
-class Cuccia extends Products {
+class Toys extends Products {
+    public $material;
 
+    function __construct($name, $barcode, $price, $material){
+        parent::__construct($name, $barcode, $price);
+    }
+}
+
+class Customer {
+    public $name;
+    public $lastName;
+    public $age;
+    public $sex;
+
+    function __construct($name, $lastname, $age, $sex){
+        $this->name = $name;
+        $this->lastName = $lastName;
+        $this->age = $age;
+        $this->sex = $sex;
+    }
+}
+
+class CreditCard {
+    public $ownerName;
+    public $ownerLastName;
+    public $code;
+    public $deadLine;
+
+    function __construct($ownerName, $ownerLastName, $code, $dealine){
+        $this->ownerName = $ownerName;
+        $this->ownerLasName = $ownerLastName;
+        $this->code = $code;
+        $this->deadline = $deadLine;
+    }
 }
 
