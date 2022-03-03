86c6b0e772f9102464f25a2758a7854f6c7cea08
Adner16 <giovanni.renda16@gmail.com>
2022-03-03 16:09:13 +0100

    first commit

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
 
+$subscription = false;
+
+class Products {
+    public $name;
+    public $barCode;
+    public $price;
+    public $discount;
+    public $finalPice;
+    public function setSconto($subscription){
+        if($this->subcription == true){
+            $this->discount = 20;
+        }
+    }
+    public function getPrice($discount,$price){
+        return $this->finalPrice = $this->price *($discount/100);
+    }
+    function __construct($name, $barcode, $price, $discount){
+        $this->name = $name;
+        $this->barcode = $barCode;
+        $this->price = $price;
+        $this->discount = $discount;
+    }
+}
+
+class Cuccia extends Products {
+
+}
+
